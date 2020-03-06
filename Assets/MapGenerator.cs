6dc7c3114e3324f344d00c77de9370711a4d9376
Max Stewart <39015672+MaxStewart@users.noreply.github.com>
2020-03-07 11:15:50 +1300

    Continued progress towards landmass generation

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -9,13 +9,41 @@ public class MapGenerator : MonoBehaviour
     public int mapHeight;
     public float noiseScale;
 
+    public int octave;
+    [Range(0,1)]
+    public float persistance;
+    public float lacurnarity;
+
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octave, persistance, lacurnarity, offset);
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    private void OnValidate()
+    {
+        if(mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if(mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacurnarity < 1)
+        {
+            lacurnarity = 1;
+        }
+        if (octave < 0)
+        {
+            octave = 0;
+        }
     }
 }
